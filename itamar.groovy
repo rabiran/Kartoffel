de2c495d9dccca2b2351f569edb349ccee9e9156
galshiff <galshiff@gmail.com>
2018-03-07 15:23:58 +0200

    unit test v1

--- itamar.groovy
+++ itamar.groovy
@@ -1,11 +1,15 @@
 pipeline {
-    agent {
-        label 'backend-dev'
-    }
-    steps {
-        echo GIT_COMMIT
-        echo BUILD_TAG
-        sh 'npm install'
-        sh 'npm test'
+    agent none
+    stages {
+        stage ('unit-test')
+            agent {
+                label 'backend-dev'
+            }
+            steps {
+                echo GIT_COMMIT
+                echo BUILD_TAG
+                sh 'npm install'
+                sh 'npm test'
+            }
     }
 }
