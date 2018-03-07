7f6da00e119ba115f73894fdd011b5089b5bc732
galshiff <galshiff@gmail.com>
2018-03-07 16:09:18 +0200

    v4

--- itamar.groovy
+++ itamar.groovy
@@ -1,12 +1,11 @@
 pipeline {
+    agent none
     stages {
         stage ('unit-testing') {
             agent {
                 label 'backend-dev'
             }
             steps {
-                echo GIT_COMMIT
-                echo BUILD_TAG
                 sh 'npm install'
                 sh 'npm test'
             }
