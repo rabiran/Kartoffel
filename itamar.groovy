69c12db8979bdb94fd8b66b562b83140c230f89c
galshiff <galshiff@gmail.com>
2018-03-07 14:48:45 +0200

    change itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -1,15 +1,3 @@
-def gitUrl = 'https://github.com/AllKinds/Kartoffel'
-
-job('Kartoffel-Unit-Testing') {
-    scm {
-        git(gitUrl)
-    }
-    triggers {
-        scm('*/5 * * * *')
-    }
-    steps {
-        shell('cd /var/lib/jenkins/workspace/Kartofell')
-        shell('npm install')
-        shell('npm test')
-    }
+pipeline {
+    
 }
