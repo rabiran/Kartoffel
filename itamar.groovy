a382f748f4a95d5459e4be9e09bf19fc5c994d19
DavidHeymann <32355522+DavidHeymann@users.noreply.github.com>
2018-08-02 10:39:42 +0300

    Update itamar.groovy
    In order for the deploy stage to work, we added environment variables to the "npm start" command

--- itamar.groovy
+++ itamar.groovy
@@ -23,7 +23,7 @@ pipeline {
             steps {
                 sh 'sudo service mongod start'
                 sh 'npm install'
-                sh 'npm start &'
+                sh 'NODE_ENV=dev MONGODB_URI=mongodb://localhost:27017/kartoffel SESSION_SECRET=just_an_example npm start &'
             }
         }    
     }
