62762271d6ef23ff78812a7a6f51a21a637f96d2
davidHeymann <heymann.david@gmail.com>
2019-01-17 11:34:47 +0200

    fix filterPersonDomainUsers

--- src/person/person.utils.ts
+++ src/person/person.utils.ts
@@ -3,12 +3,13 @@ import { IPerson } from './person.interface';
 import { IDomainUser } from '../domainUser/domainUser.interface';
 
 export function filterPersonDomainUsers(person: IPerson): IPerson {
+  const filterField = ['uniqueID', 'adfsUID'];
   const changePerson = { ...person }; 
   changePerson.secondaryDomainUsers = <IDomainUser[]>(<IDomainUser[]>changePerson.secondaryDomainUsers).map((domainUser: IDomainUser) => {
-    return filterObjectByKeys(domainUser, ['uniqueID', 'adfsUID']);
+    return filterObjectByKeys(domainUser, filterField);
   });
   if (changePerson.primaryDomainUser) {
-    changePerson.primaryDomainUser = <IDomainUser>filterObjectByKeys(changePerson.primaryDomainUser, ['uniqueID', 'adfsUID']);
+    changePerson.primaryDomainUser = <IDomainUser>filterObjectByKeys(changePerson.primaryDomainUser, filterField);
   }
   return changePerson;
 }
