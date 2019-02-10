276f9ea8d028419e41bd15268f28ea9d816f8483
DavidHeymann <32355522+DavidHeymann@users.noreply.github.com>
2019-02-10 12:52:00 +0200

    Identifier api (#82)
    * Add api and a compatible function to get person by personal number or identity card
    
    * Add validation to identityCard and change tests
    
    * Add validation in create person
    
    * Add tests
    
    * fixed comment
    
    * delete only from tests
    
    * add new route to swagger

--- src/config/db-enums.ts
+++ src/config/db-enums.ts
@@ -1,4 +1,5 @@
-export const RANK = ['rookie', 'champion', 'ultimate', 'mega'];
+// The first element is the default value of who need rank
+export const RANK = ['unknown', 'rookie', 'champion', 'ultimate', 'mega'];
 // the last element makes the 'rank' field required
 export const ENTITY_TYPE = ['digimon', 'tamar'];
 // the first element is the default value
