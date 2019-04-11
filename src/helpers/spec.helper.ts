a895677be47cf58f9a31a517816454112dae5427
DavidHeymann <heymann.david@gmail.com>
2019-04-11 19:18:08 +0300

    Add useCreateIndex to replace ensureIndexes  in createdIndexes becouse is depricated and mongoose uses it

--- src/helpers/spec.helper.ts
+++ src/helpers/spec.helper.ts
@@ -127,7 +127,7 @@ async function removeAllDocuments(modelNames: string[]) {
 }
 
 before(async () => {
-  await mongoose.connect(process.env.MONGODB_TEST_URI, { useNewUrlParser: true, useFindAndModify: false });
+  await mongoose.connect(process.env.MONGODB_TEST_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
   const modelNames: string[] = mongoose.modelNames();
   await cleanDatabase(modelNames);
 });
--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ dotenv.config({ path: '.env' });
 (<any>mongoose).Promise = Promise;
 
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false })
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => console.log('successfully connected to the database'))
   .catch(err => console.error(err));
 }
