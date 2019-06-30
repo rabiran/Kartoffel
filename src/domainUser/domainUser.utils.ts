8b596f33cac5385973b2a572532dec6d871c6e7c
elad biran <eladex@gmail.com>
2019-06-30 13:38:25 +0300

    updated docs & using validation error in domain user helper

--- src/domainUser/domainUser.utils.ts
+++ src/domainUser/domainUser.utils.ts
@@ -1,11 +1,12 @@
 import { IDomainUser } from './domainUser.interface';
 import { DomainSeperator } from '../utils';
 import { isLegalUserString } from './domainUser.validators';
+import { ValidationError } from '../types/error';
 
 
 export function userFromString(uniqueID: string): IDomainUser {
   if (!isLegalUserString(uniqueID)) {
-    throw new Error(`${uniqueID} is illegal user representation`);
+    throw new ValidationError(`${uniqueID} is illegal user representation`);
   }
   const splitted = uniqueID.split(DomainSeperator);
   const name = splitted[0], domain = splitted[1];
