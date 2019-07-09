cd6420f6bd253c7b66b6f4754e43280cb01cfa6c
elad biran <eladex@gmail.com>
2019-07-09 23:06:29 +0300

    added rotating file transport to logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,17 +1,43 @@
 import * as os from 'os';
 import * as winston from 'winston';
+import * as winstonRotateFile from 'winston-daily-rotate-file';
 const ESWinston = require('winston-elasticsearch');
+// const WinstonRotateFile = require('winston-daily-rotate-file');
 const indexTemplateMapping = require('winston-elasticsearch/index-template-mapping.json');
 
 const serviceName = process.env.SERVICE_NAME || 'kartoffel';
 const indexPrefix = process.env.ES_INDEX_PREFIX || serviceName;
 indexTemplateMapping.index_patterns = `${indexPrefix}-*`; 
 
+// log levels
+export enum LOG_LEVEL {
+  ERROR = 'error',
+  WARN = 'warn',
+  INFO = 'info',
+  VERBOSE = 'verbose',
+  DEBUG = 'debug',
+  SILLY = 'silly',
+}
+
 const logger = winston.createLogger({
   defaultMeta: { service: serviceName, hostname: os.hostname() },
 });
 
-if (process.env.ES_HOSTS) {
+const format = winston.format.combine(
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss',
+  }),
+  winston.format.json());
+
+if (process.env.LOG_FILE_NAME) {
+  logger.add(new winstonRotateFile({
+    format,
+    level: LOG_LEVEL.INFO,
+    datePattern: 'YYYY-MM-DD',
+    filename: process.env.LOG_FILE_NAME,
+    dirname: process.env.LOG_FILE_DIR || '.',
+  }));
+} else if (process.env.ES_HOSTS) {
   const esClientOpts = {
     hosts: process.env.ES_HOSTS.split(','),
   };
@@ -26,28 +52,14 @@ if (process.env.ES_HOSTS) {
   logger.add(esTransport);
 } else {
   const winstonConsole = new winston.transports.Console({
+    format,
     level: 'silly',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      winston.format.json()
-    ),
   });
   logger.add(winstonConsole);
 }
+
 
 export const log = (severity: string, meta: any) => {
   const { message, ...other } = meta;
   logger.log(severity, message, other);
 };
-
-export enum LOG_LEVEL {
-  ERROR = 'error',
-  WARN = 'warn',
-  INFO = 'info',
-  VERBOSE = 'verbose',
-  DEBUG = 'debug',
-  SILLY = 'silly',
-}
-
