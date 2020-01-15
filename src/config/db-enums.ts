d306245f932363577139cc8cc2eefa6a287dcf0f
Eli-BB8 <47026823+Eli-BB8@users.noreply.github.com>
2020-01-15 17:52:12 +0200

    Dbenums (#128)
    * (feat) update db.enums
    
    * ,
    
    * mamatzav

--- src/config/db-enums.ts
+++ src/config/db-enums.ts
@@ -9,8 +9,17 @@ export const ENTITY_TYPE = ['digimon', 'agumon', 'tamar'];
 // the first element is the default value
 export const RESPONSIBILITY = ['none', 'HR', 'SO'];
 // If there is no ADFS you will write an empty string value ('')
-export const DOMAIN_MAP = [['rabiran.com','rabiranuid'], ['somedomain.com', 'somedomainuid'], ['jello.com', 'jellouid'], ['jello2.com', 'jellouid'], ['yoda.sw', '']];
+export const DOMAIN_MAP = [
+  ['rabiran.com', 'rabiranuid'],
+  ['somedomain.com', 'somedomainuid'],
+  ['jello.com', 'jellouid'],
+  ['jello2.com', 'jellouid'],
+  ['yoda.sw', ''],
+  ['turtle.com', ''],
+  ['donatelo.turtle.com', ''],
+  ['rafael.turtle.com', ''],
+];
 // the first element is the default value
-export const SERVICE_TYPE = ['A' ,'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+export const SERVICE_TYPE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-export const DATA_SOURCE = ['dataSource1', 'dataSource2'];
+export const DATA_SOURCE = ['dataSource1', 'dataSource2','aka', 'es_name', 'ads_name', 'adNN_name', 'nvSQL_name', 'lmn_name', 'mdn_name', 'mm_name', 'city_name'];
