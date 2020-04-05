2d4b4b8ac92fdf3c4366c74f24716d2b653ae7ff
eladex <eladex@gmail.com>
2020-04-05 23:03:49 +0300

    remove middleware and add docs

--- src/helpers/queryTransform.ts
+++ src/helpers/queryTransform.ts
@@ -2,54 +2,44 @@ import { Request, Response, NextFunction } from 'express';
 import { KeyMap, ValueMap, transformKeys, ObjectValueMap,
   transformValues, filterObjectByKeys } from '../utils';
 
-interface QueryMiddlewareOpts {
-  paramsRenameMap?: KeyMap;
-  filterParams?: string[];
+interface TransformOpts {
+  renameMap?: KeyMap;
+  selectFields?: string[];
   defaults?: ValueMap;
-  valueAliases?: ObjectValueMap;
+  valuesAliases?: ObjectValueMap;
 }
 
 /**
- * Transform the query object: 
- * <> filter object fields
- * <> rename object keys
- * <> apply default values
- * <> swap value aliases 
- * @param originalQuery 
- * @param queryParamsRenameMap 
- * @param filterQueryParams 
- * @param queryDefaults 
- * @param queryValuesAliases 
- */
-function transformQuery(
-  originalQuery: object,
-  queryParamsRenameMap: KeyMap = {},
-  filterQueryParams: string[] = null,
-  queryDefaults: ValueMap = {}, 
-  queryValuesAliases: ObjectValueMap = {}) {
-  // filter params
-  const filtered = filterObjectByKeys(originalQuery, filterQueryParams, true);
-  // rename query params
-  const renamed = transformKeys(filtered, queryParamsRenameMap);
-  // apply defaults
-  const withDefaults = { ...queryDefaults, ...renamed };
-  // apply values aliases and return 
-  return transformValues(withDefaults, queryValuesAliases);
-}
-
-/**
+ * Extracts specific fields from an object (case insesitive), then renames them 
+ * according to the given `renameMap`, then adds default values to missing fields
+ * using the given `defaults` and finally uses the `valuesAliases` to replace 
+ * some field values' aliases with the 'real' values.
+ * Returns a new object.
  * 
- * @param middlewareOpts object with keys: 
- * @param `middlewareOpts.paramsRenameMap` - object that maps query params original names to new names.
- * @param `middlewareOpts.filterParams` - array of allowed query params, defaults to `null` - which is not filtering.
- * @param `middlewareOpts.defaults` - default values for params.
- * @param `middlewareOpts.valueAliases` - objects that maps param to it's alieses map - which is an object by itself;
- * this object maps values of the param to a new value.
+ * @param original original object
+ * @param transformOpts object with the following fields:  
+ * @property `selectFields` - fields to select - returned object will have only 
+ *        a subset of these keys, omitting this option will select all fields
+ * @property `renameMap` - maps original fields names to new names
+ * @property `defaults` - maps a field to its default value (if there is)
+ * @property `valuesAliases` - maps a field to its alias map - which by itslef maps 
+ * a field original value ('alias') to a new value
  */
-export const makeMiddleware = (middlewareOpts: QueryMiddlewareOpts) => 
-  (req: Request, res: Response, next: NextFunction) => {
-    const { paramsRenameMap, filterParams, defaults, valueAliases } = middlewareOpts;
-    req.query = transformQuery(req.query, paramsRenameMap, filterParams, defaults, valueAliases);
-    next();
-  };
-
+export function transformQueryFields(
+  original: object, transformOpts: TransformOpts = {}) {
+  const {
+    selectFields = null,
+    renameMap = {},
+    defaults = {}, 
+    valuesAliases = {},
+  } = transformOpts;
+  // filter fields
+  const filtered = selectFields ? filterObjectByKeys(original, selectFields, true) :
+    { ...original };
+  // rename fields
+  const renamed = transformKeys(filtered, renameMap);
+  // apply defaults
+  const withDefaults = { ...defaults, ...renamed };
+  // apply values aliases and return 
+  return transformValues(withDefaults, valuesAliases);
+}
