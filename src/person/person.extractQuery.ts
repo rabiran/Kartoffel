5798912a600bad3df29e779d3c86aa0ff5b93884
eladex <eladex@gmail.com>
2021-01-17 16:50:25 +0200

    fix(personExtractQuery): return filters object with correct key
    corrected key: hierarchyPath ->  underGroupId

--- src/person/person.extractQuery.ts
+++ src/person/person.extractQuery.ts
@@ -1,21 +1,20 @@
 import { config } from '../config/config';
-import { extract, replaceValues } from '../utils';
+import { extract, replaceValues, pickSingleValue } from '../utils';
 import Query from '../types/Query';
-import { PersonFilter, PersonSearchQuery } from './person.controller';
-import singleValue from '../helpers/makeSingleValue';
+import { PersonFilters, PersonSearchQuery } from './person.controller';
 
 const { aliases: { persons: personAliases }, defaults: { persons: personDefaults } } = config.queries;
 
-const extractFilterKeys: (keyof PersonFilter)[] = ['currentUnit', 'domainUsers.dataSource', 
+const extractFilterKeys: (keyof PersonFilters)[] = ['currentUnit', 'domainUsers.dataSource', 
   'entityType', 'job', 'rank', 'responsibility', 'serviceType', 'status', 'underGroupId'];
 const extractSearchKeys: (keyof PersonSearchQuery)[] = [...extractFilterKeys, 'fullName'];
 
 
-export const extractFilters = (query: Query<Partial<PersonFilter>>): Partial<PersonFilter> => {
+export const extractFilters = (query: Query<Partial<PersonFilters>>): Partial<PersonFilters> => {
   const { underGroupId, ...rest } = applyDefaultsAndAliases(extract(query, extractFilterKeys));
   return {
     ...!!underGroupId && {
-      hierarchyPath: singleValue(underGroupId),
+      underGroupId: pickSingleValue(underGroupId),
     },
     ...rest,
   };
@@ -26,11 +25,11 @@ export const extractSearchQuery = (query: Query<Partial<PersonSearchQuery>>): Pa
   const filtersWithAliases = extractFilters(filters);
   return {
     ...filtersWithAliases,
-    fullName: singleValue(fullName),
+    fullName: pickSingleValue(fullName),
   };
 };
 
-const applyDefaultsAndAliases = (query: Query<Partial<PersonFilter>>): Query<Partial<PersonFilter>> => {
+const applyDefaultsAndAliases = (query: Query<Partial<PersonFilters>>): Query<Partial<PersonFilters>> => {
   const withDefaults = { ...personDefaults, ...query };
   return replaceValues(withDefaults, personAliases);
 };
