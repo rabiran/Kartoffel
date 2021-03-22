a6a1e7f7fc9c39d3827273600052f66d0acf197d
elad biran <eladex@gmail.com>
2021-03-22 18:39:37 +0200

    chore: delete uneeded files

--- src/group/organizationGroup/organizationGroup.scopePolicyService.ts
+++
@@ -1,8 +0,0 @@
-import { GroupExcluderQuery } from './organizationGroup.excluder.query';
-import { ScopePolicyService } from '../../scopes/ScopePolicyService';
-import { IOrganizationGroup } from './organizationGroup.interface';
-import scopePolicy from '../../config/scopeRules';
-
-const { scopes, rules: { organizationGroup } } = scopePolicy;
-
-export default new ScopePolicyService<IOrganizationGroup, GroupExcluderQuery>(organizationGroup, scopes);
