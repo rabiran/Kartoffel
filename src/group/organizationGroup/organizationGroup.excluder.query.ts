d9e92091daebf9e8da78b22e82dfaa1a8bc323d5
elad biran <eladex@gmail.com>
2021-03-22 18:27:48 +0200

    feature(group+person): add excluder checker

--- src/group/organizationGroup/organizationGroup.excluder.query.ts
+++ src/group/organizationGroup/organizationGroup.excluder.query.ts
@@ -1,4 +1,25 @@
+import { IOrganizationGroup } from './organizationGroup.interface';
+
 export type GroupExcluderQuery = {
   hierarchy: string[];
   akaUnit: string[];
 };
+
+export function statisfyExcluder(group: IOrganizationGroup, excluder: Partial<GroupExcluderQuery>) {
+  const { hierarchy = [], ...rest } = excluder;
+  // check if group is under one of the excluded hierarchies
+  for (const hierarchyPath of hierarchy) {
+    const groupFullPath = [...group.hierarchy, group.name].join('/');
+    if (groupFullPath.startsWith(hierarchyPath)) {
+      return true;
+    }
+  }
+  // check if group statisfied the other excluders
+  for (const key of Object.keys(rest)) {
+    const excluded: string[] = rest[key];
+    if (excluded.includes(group[key])) {
+      return true;
+    }
+  }
+  return false;
+}
--- src/person/person.excluder.query.ts
+++ src/person/person.excluder.query.ts
@@ -1,7 +1,26 @@
+import { IPerson } from './person.interface';
+
 export type PersonExcluderQuery = {
-  hierarchy: string[];
+  hierarchy: string[]; // array of hierarchy strings. e.g: ['a/b/c', 'a/g']
   rank: string[];
   currentUnit: string[];
 };
+
+export function statisfyExcluder(person: IPerson, excluder: Partial<PersonExcluderQuery>) {
+  const { hierarchy = [], ...rest } = excluder;
+  for (const key of Object.keys(rest)) {
+    const excluded: string[] = rest[key];
+    if (excluded.includes(person[key])) {
+      return true;
+    }
+  }
+  const hierarchyPath = person.hierarchy.join('/');
+  for (const h of hierarchy) {
+    if (hierarchyPath.startsWith(h)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 export default PersonExcluderQuery;
